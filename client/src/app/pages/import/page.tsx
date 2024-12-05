1f576ba75d87a8725d3a3b6878f5e82244db15d7
Islahuddin <islahuddin.me@gmail.com>
2024-12-05 12:12:36 +0500

    Porject init

---
+++ client/src/app/pages/import/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const imports = (props: Props) => {
+    return <div>imports</div>;
+};
+
+export default imports;
---
+++ client/src/app/pages/summary/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const purchase = (props: Props) => {
+    return <div>purchase</div>;
+};
+
+export default purchase;
